3ebce251bc0395bdce672a1a65ebc1ea95ac4bec
Linh Bui <buikhanhlinhtl@gmail.com>
2023-06-06 19:48:04 +0700

    n commit

---
+++ store/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Product
+
+# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'price', 'stock', 'category', 'modified_date', 'is_available')
+    prepopulated_fields = {'slug': ('product_name',)}
+
+# class VariationAdmin(admin.ModelAdmin):
+#     list_display = ('product', 'variation_category', 'variation_value', 'is_active')
+#     list_editable = ('is_active',)
+#     list_filter = ('product', 'variation_category', 'variation_value')
+
+admin.site.register(Product, ProductAdmin)
+# admin.site.register(Variation, VariationAdmin)
+# admin.site.register(ReviewRating)
